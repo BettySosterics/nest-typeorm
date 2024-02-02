762032b313ce88148cf3ca724ab95fa49359d65a
Betty Sosterics <betty.sosterics@gmail.com>
2024-02-02 10:11:34 +0100

    Fix imagePath to userId

--- src/auth/controllers/user.controller.ts
+++ src/auth/controllers/user.controller.ts
@@ -1,10 +1,6 @@
 import {
-  Body,
   Controller,
-  FileTypeValidator,
   Get,
-  MaxFileSizeValidator,
-  ParseFilePipe,
   Post,
   Request,
   Res,
@@ -14,16 +10,11 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import path from 'path';
 import { Observable, of, switchMap } from 'rxjs';
 import { UpdateResult } from 'typeorm';
 import { JwtGuard } from '../guards/jwt.guard';
 import { RolesGuard } from '../guards/roles.guard';
 import { UserService } from '../services/user.service';
-
-interface FileParams {
-  fileName: string;
-}
 
 @Controller('user')
 export class UserController {
@@ -66,6 +57,7 @@ export class UserController {
     const fileName = file?.filename;
 
     if (!fileName) return Promise.resolve({ error: 'File must be an image' });
+    await this.UserService.updateUserImageById(req.user.id, fileName);
   }
 
   @UseGuards(JwtGuard)
@@ -90,49 +82,3 @@ export class UserController {
     );
   }
 }
-//   @UseGuards(JwtGuard, RolesGuard)
-//   @Post('upload')
-//   @UseInterceptors(
-//     FileInterceptor('file', {
-//       storage: diskStorage({
-//         destination: './uploads',
-//         filename: (req, file, cb) => {
-//           cb(null, file.originalname);
-//         },
-//       }),
-//     }),
-//   )
-//   uploadImage(
-//     @UploadedFile() file: Express.Multer.File,
-//     @Request() req,
-//   ): Observable<UpdateResult | { error: string }> {
-//     const fileName = file?.filename;
-
-//     if (!fileName) return of({ error: 'File must be an image' });
-
-//     const imagesFolderPath = join(process.cwd(), 'images');
-//     const fullImagePath = join(imagesFolderPath + '/' + file.filename);
-//   }
-//   return isFileExtensionSafe(fullImagePath).pipe(
-//     switchMap((isFileLegit: boolean) => {
-//       if (isFileLegit) {
-//         const userId = req.user.id;
-//         return this.UserService.updateUserImageById(userId, fileName);
-//       }
-//       removeFile(fullImagePath);
-//       return of({ error: 'File content does not match the extension!' });
-//     }),
-//     );
-//   }
-
-//   @UseGuards(JwtGuard)
-//   @Get('image')
-//   findImage(@Request() req, @Res() res): Observable<Object> {
-//     const userId = req.user.id;
-//     return this.UserService.findImageNameByUserId(userId).pipe(
-//       switchMap((imageName: string) => {
-//         return of(res.sendFile(imageName, { root: './uploads' }));
-//       }),
-//     );
-//   }
-// }
